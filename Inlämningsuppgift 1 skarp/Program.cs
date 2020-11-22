cb60451bf247dc755d53c205219218065b8f30c6
Håkan Eriksson <badfish.paint@gmail.com>
2020-11-22 18:04:08 +0100

    User input för kalkylatordelen är klar.

--- Inlämningsuppgift 1 skarp/Program.cs
+++ Inlämningsuppgift 1 skarp/Program.cs
@@ -10,16 +10,34 @@ namespace Inlämningsuppgift_1_skarp
             Console.Title = "Campus Mölndal";
             
             List<int> changes = new List<int>();
+            bool keepGoing = false;
             int result = 0;
 
+            Console.WriteLine("Hello and welcome to the calculator!");
+            System.Threading.Thread.Sleep(1800);
+            Console.Clear();
+
+            do
+            {
+                Console.WriteLine("Enter first operator: ");
+                string operatorOne = Console.ReadLine();
+                char one = Convert.ToChar(operatorOne);
+                Console.WriteLine("Enter second operator: ");
+                string operatorTwo = Console.ReadLine();
+                char two = Convert.ToChar(operatorTwo);
+
+                Console.WriteLine("Enter first term: ");
+                int termOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second term: ");
+                int termTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter third term: ");
+                int termThree = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Enter first operator: ");
-            string operatorOne = Console.ReadLine();
-            char one = Convert.ToChar(operatorOne);
-            Console.WriteLine("Enter second operator: ");
-            string operatorTwo = Console.ReadLine();
-            char two = Convert.ToChar(operatorTwo);
+
+            } while (keepGoing == true);
+
+            
 
 
 
