911223317c0aa0b311970e5faa39c77b981c1b97
Håkan Eriksson <badfish.paint@gmail.com>
2020-11-22 17:52:13 +0100

    Små förändringar endast för att öva och få in en rutin att alltid pusha till Git när förändringar sker.

--- Inlämningsuppgift 1 skarp/Program.cs
+++ Inlämningsuppgift 1 skarp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlämningsuppgift_1_skarp
 {
@@ -7,6 +8,12 @@ namespace Inlämningsuppgift_1_skarp
         static void Main(string[] args)
         {
             Console.Title = "Campus Mölndal";
+            
+            List<int> changes = new List<int>();
+            int result = 0;
+
+
+
             Console.WriteLine("Enter first operator: ");
             string operatorOne = Console.ReadLine();
             char one = Convert.ToChar(operatorOne);
