a06f2a4c24888bb5599458ad6dbbc7036c743173
Håkan Eriksson <badfish.paint@gmail.com>
2020-11-22 17:32:50 +0100

    Add project files.

---
+++ Inlämningsuppgift 1 skarp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämningsuppgift_1_skarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
